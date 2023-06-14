3dd8f3dee4fa42777fb764b157eeb162ac130397
JingYi <136475454+Jingyi14yup@users.noreply.github.com>
2023-06-14 15:42:24 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,9 +1,35 @@
-let pic = document.getElementById("profile-picture");
+//Variables
 
-document.addEventListener("scroll", function () {
-  if (window.scrollY > 5) {
-    pic.style.borderRadius = "0%";
-  } else {
-    pic.style.borderRadius = "100%";
-  }
-});
+
+//Comparisons
+
+//If statements
+
+//Functions
+
+//Controlling HTML with JavaScript
+
+var img = document.getElementById("gallery-img");
+var prev = document.getElementById("prev");
+var next = document.getElementById("next");
+
+var images = ["smiling emoji.webp" , "COVER-Music.png" , "fit.jpg"][;
+
+var currentimage = 0;
+
+function changimage(e) {
+    if (e.target.id == "next" && currentimage !== images.length -1) {
+        //increment the images (in other words, go to the next one)
+        currentimage = currentimage +1;
+}
+
+if (e.target.id == "prev" && currentimage !== 0) {
+    //logic goes in here
+    currentimage = currentimage -1;
+}
+
+img.setAttribute("src", "images/" + images[currentimage]);
+}
+
+prev.addEventListener("click", changimage);
+next.addEventListener("click", changimage);
